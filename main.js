d06bb3b99444763c8a9d3513f40dbf043f2f10eb
kanzhuchengbi <142250020+kanzhuchengbi@users.noreply.github.com>
2026-02-08 01:09:23 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ const glados = async () => {
   try {
     const headers = {
       'cookie': cookie,
-      'referer': 'https://glados.cloud/console/checkin',
+      'referer': 'https://glados.space/console/checkin',
       'user-agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
     }
-    const checkin = await fetch('https://glados.cloud/api/user/checkin', {
+    const checkin = await fetch('https://glados.space/api/user/checkin', {
       method: 'POST',
       headers: { ...headers, 'content-type': 'application/json' },
       body: '{"token":"glados.one"}',
     }).then((r) => r.json())
-    const status = await fetch('https://glados.cloud/api/user/status', {
+    const status = await fetch('https://glados.space/api/user/status', {
       method: 'GET',
       headers,
     }).then((r) => r.json())
